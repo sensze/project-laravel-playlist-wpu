50053243d2bb2451eba23416dd003d2367515460
ifkusyoba <ifkusyobaa@gmail.com>
2022-06-01 18:11:03 +0700

    4. Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ifku | About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>Halaman About</h1>
-    <h3><?php echo $name ?></h3>
-    <p><?php echo $email?></p>
-    <img src="img/<?= $image ?>" alt="<?= $name ?>" width="200">
-</body>
-</html>
+    <h3>{{$name}}</h3>
+    <p>{{$email}}</p>
+    <img src="img/{{$image}}" alt="[{$name}}" width="200">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,67 @@ use Illuminate\Support\Facades\Route;
 */
 //Route home
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => 'Home'
+    ]);
 });
 //Route about
 Route::get('/about', function () {
     return view('about', [
+        "title" => 'About',
         "name" => "Ifku Syoba",
         "email" => "ifkusyobaa@gmail.com",
         "image" => "profile_picture.jpg"
     ]);
 });
+
+
+
 //Route Blog
 Route::get('/posts', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "judul" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ifku Syoba",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione consectetur laboriosam, vel ex maxime ipsum vitae voluptas est minima corporis cumque ea dolorem mollitia necessitatibus nihil earum doloribus accusantium nam natus beatae fugit expedita deserunt nobis? Iusto rem tempora quia aspernatur odio, modi ratione saepe cumque similique vero eum obcaecati? Sit reiciendis corporis aspernatur nostrum blanditiis, non ea possimus enim architecto, quis consequuntur aliquid incidunt ex soluta dolor recusandae! Architecto suscipit labore rem explicabo ex temporibus quae ratione corporis nobis."
+        ],
+        [
+            "judul" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Agus Ari",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione consectetur laboriosam, vel ex maxime ipsum vitae voluptas est minima corporis cumque ea dolorem mollitia necessitatibus nihil earum doloribus accusantium nam natus beatae fugit expedita deserunt nobis? Iusto rem tempora quia aspernatur odio, modi ratione saepe cumque similique vero eum obcaecati? Sit reiciendis corporis aspernatur nostrum blanditiis, non ea possimus enim architecto, quis consequuntur aliquid incidunt ex soluta dolor recusandae! Architecto suscipit labore rem explicabo ex temporibus quae ratione corporis nobis."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts,
+    ]);
 });
+ Route::get('/posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "judul" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ifku Syoba",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione consectetur laboriosam, vel ex maxime ipsum vitae voluptas est minima corporis cumque ea dolorem mollitia necessitatibus nihil earum doloribus accusantium nam natus beatae fugit expedita deserunt nobis? Iusto rem tempora quia aspernatur odio, modi ratione saepe cumque similique vero eum obcaecati? Sit reiciendis corporis aspernatur nostrum blanditiis, non ea possimus enim architecto, quis consequuntur aliquid incidunt ex soluta dolor recusandae! Architecto suscipit labore rem explicabo ex temporibus quae ratione corporis nobis."
+        ],
+        [
+            "judul" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Agus Ari",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione consectetur laboriosam, vel ex maxime ipsum vitae voluptas est minima corporis cumque ea dolorem mollitia necessitatibus nihil earum doloribus accusantium nam natus beatae fugit expedita deserunt nobis? Iusto rem tempora quia aspernatur odio, modi ratione saepe cumque similique vero eum obcaecati? Sit reiciendis corporis aspernatur nostrum blanditiis, non ea possimus enim architecto, quis consequuntur aliquid incidunt ex soluta dolor recusandae! Architecto suscipit labore rem explicabo ex temporibus quae ratione corporis nobis."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+     return view('post', [
+         "title" => "Single Post",
+         "post" => $new_post
+     ]);
+ });
